cd9154f0b3249d3b65088e08a66dbf92b3f89bd3
movingnewboy <moovingnewboy@gmail.com>
2024-12-14 00:12:14 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -8,6 +8,7 @@ class Config(object):
   BOT_USERNAME = os.environ.get("BOT_USERNAME", "QL_Series_Bot")
   LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002475103221"))
   UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002250117533")
+  CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002439773458"))
 
   DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1002038837206")) #Common
   SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "modijiurl.com")
