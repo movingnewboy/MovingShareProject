ea2b65f6f05cb78258840697827d4f0d3ef7d867
movingnewboy <moovingnewboy@gmail.com>
2025-01-25 22:24:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -39,7 +39,7 @@ from handlers.force_sub_handler import (
 from handlers.broadcast_handlers import main_broadcast_handler
 from handlers.save_media import (
     save_media_in_channel
-    # save_batch_media_in_channel
+    save_batch_media_in_channel
 )
 
 # Dictionary to store media for users
