19604e5e1bbd16d8af81fe34edada1aefe9629e8
movingnewboy <moovingnewboy@gmail.com>
2025-01-26 19:04:18 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -104,7 +104,7 @@ async def not_joined(client: Client, message: Message):
                 ],
                 [
                     InlineKeyboardButton(text = 'Try Again',
-                    url = f"https://t.me/{client.username}?start={message.command[1]}")
+                    url = f"https://t.me/{Config.BOT_USERNAME}?start={message.command[1]}")
                 ]
             ]
         ),
