4325490e29658edbfd002cd0c942062210cbc762
movingnewboy <moovingnewboy@gmail.com>
2025-05-04 07:05:28 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -5,7 +5,7 @@ class Config(object):
   API_HASH = os.environ.get("API_HASH", "f19aed00b0c74abed0359016afc1733f")
   
   BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-  BOT_USERNAME = os.environ.get("BOT_USERNAME", "New_Theatre_Movies_Bot")
+  BOT_USERNAME = os.environ.get("BOT_USERNAME", "New1_Theatre_Movies_Bot")
   LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001884285982"))
   UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001859384286")
   CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002478513434"))
